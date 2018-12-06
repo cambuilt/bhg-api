6bac166eea60e66a00093e6f7a8c3a0ec59ab5ab
Cameron Conway <support@cambuilt.com>
2018-12-06 17:07:10 -0500

    'models'

---
+++ bhg/Models/Place.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace bhg.Models
+{
+    public class Place
+    {
+        public int PlaceId { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+        public decimal Latitude { get; set; }
+        public decimal Longitude { get; set; }
+        public string Notes { get; set; }
+        public DateTime CreateDate { get; }
+        public DateTime? ModDate { get; }
+    }
+}
---
+++ bhg/Models/TreasureMap.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace bhg.Models
+{
+    public class TreasureMap
+    {
+        public TreasureMap()
+        {
+            // Order = new HashSet<Order>();
+        }
+
+        public string TreasureMapId { get; }
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Area { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Author { get; set; }
+        public decimal Latitude { get; set; }
+        public decimal LatitudeDelta { get; set; }
+        public decimal Longitude { get; set; }
+        public decimal LongitudeDelta { get; set; }
+        public DateTime CreateDate { get; }
+        public DateTime? ModDate { get; }
+    }
+}
